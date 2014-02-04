53a5233e72a6a1d73da220aaf1029165b8145f6d
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-02-04 01:29:17 -0500

    Cleaned up code. Still no solution to missing radial inputs.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,59 +25,41 @@ var fighterNickNames = [
 
 // Would You Rather Question One
  $("#set1option1").click(function () {
- 
 	$('input:radio[name=optionsRadiosOne]:nth(0)').attr('checked',true);
-	//$('input:radio[name=One]')[0].checked = true;
     });
 
  $("#set1option2").click(function () {
- 
 	$('input:radio[name=optionsRadiosOne]:nth(1)').attr('checked',true);
-	//$('input:radio[name=optionsRadiosOne]')[1].checked = true;
     });
 
  $("#set1option3").click(function () {
- 
 	$('input:radio[name=optionsRadiosOne]:nth(2)').attr('checked',true);
-	//$('input:radio[name=optionsRadiosOne]')[2].checked = true;
     });
 
 // Would You Rather Question Two
  $("#set2option1").click(function () {
- 
 	$('input:radio[name=optionsRadiosTwo]:nth(0)').attr('checked',true);
-	//$('input:radio[name=optionsRadiosTwo]')[0].checked = true;
     });
 
  $("#set2option2").click(function () {
- 
 	$('input:radio[name=optionsRadiosTwo]:nth(1)').attr('checked',true);
-	//$('input:radio[name=optionsRadiosTwo]')[1].checked = true;
     });
 
  $("#set2option3").click(function () {
- 
 	$('input:radio[name=optionsRadiosTwo]:nth(2)').attr('checked',true);
-	//$('input:radio[name=optionsRadiosTwo]')[2].checked = true;
     });
 
 // Would You Rather Question Three
  $("#set3option1").click(function () {
- 
 	$('input:radio[name=optionsRadiosThree]:nth(0)').attr('checked',true);
-	//$('input:radio[name=optionsRadiosThree]')[0].checked = true;
     });
 
  $("#set3option2").click(function () {
- 
 	$('input:radio[name=optionsRadiosThree]:nth(1)').attr('checked',true);
-	//$('input:radio[name=optionsRadiosThree]')[1].checked = true;
     });
 
- $("#set3option3").click(function () {
- 
+ $("#set3option3").click(function () { 
 	$('input:radio[name=optionsRadiosThree]:nth(2)').attr('checked',true);
-	//$('input:radio[name=optionsRadiosThree]')[2].checked = true;
     });
 
 
